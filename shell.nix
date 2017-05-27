681361a836379480e81e4a84fe1d08e2a7308413
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-05-28 00:34:09 +0900

    properly install mbed-cli

--- shell.nix
+++ shell.nix
@@ -15,13 +15,32 @@ in with pkgs; {
         ps.jinja2
         ps.colorama
         (ps.buildPythonPackage rec {
-          name = "mbed-cli-${version}";
+          name = "${pname}-${version}";
+          pname = "mbed-cli";
           version = "1.1.1";
 
+# While we wait for the new fetchPypi to hit stable
           src = pkgs.fetchurl {
             url = "https://github.com/ARMmbed/mbed-cli/archive/${version}.zip";
+#          src = ps.fetchPypi {
+#            inherit pname version;
+#            extension = "zip";
             sha256 = "1hxp649hfpkw33i5h4yhcm0hbd5mzj8rj0i5kd9534yrsx3glba6";
           };
+
+          checkInputs = [ ps.pyyaml ps.pytest ];
+          checkPhase = ''
+            sed -i "s~- mbed~- $out/bin/mbed~" circle.yml
+            sed -i "s~& mbed~\& $out/bin/mbed~" circle.yml
+            sed -i "s/^.*&&.*$//" circle.yml # because hitting the network won't work anyway...
+            sed -i "s/popen(\['git', 'commit'/popen(['git', 'config', 'user.email', '\"you@example.com\"']);popen(['git', 'config', 'user.name', '\"Your Name\"']);popen(['git', 'commit'/" test/util.py
+            ${python.interpreter} circle_tests.py
+          '';
+
+          buildInputs = [
+            git
+            mercurial
+          ];
 
           meta = {
             homepage = "https://github.com/ARMmbed/mbed-cli";
